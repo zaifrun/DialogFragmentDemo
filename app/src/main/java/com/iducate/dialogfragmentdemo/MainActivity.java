28c790000e07974bae4682e0a3ff41f067314f68
Martin Knudsen <martinknud@gmail.com>
2016-01-27 11:38:49 +0100

    Initial commit

---
+++ app/src/main/java/com/iducate/dialogfragmentdemo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.iducate.dialogfragmentdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	//This is the event handler for the show button
+	//This is specified in the xml file.
+	public void showDialog(View v) {
+		//showing our dialog.
+		MyDialogFragment dialog = new MyDialogFragment() {
+			@Override
+			protected void positiveClick() {
+				//Here we override the methods and can now
+				//do something
+				Toast toast = Toast.makeText(getApplicationContext(),
+						"positive button clicked", Toast.LENGTH_LONG);
+				toast.show();
+			}
+			
+			@Override
+			protected void negativeClick() {
+				//Here we override the method and can now do something
+				Toast toast = Toast.makeText(getApplicationContext(),
+						"negative button clicked", Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		};
+		
+		//Here we show the dialog
+		//The tag "MyFragement" is not important for us.
+		dialog.show(getFragmentManager(), "MyFragment");
+	}
+
+}
---
+++ app/src/main/java/com/iducate/dialogfragmentdemo/MyDialogFragment.java
@@ -0,0 +1,57 @@
+package com.iducate.dialogfragmentdemo;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class MyDialogFragment extends DialogFragment {
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+		//Here we create a new dialogbuilder;
+		AlertDialog.Builder alert = new AlertDialog.Builder(
+				getActivity());
+		alert.setTitle("Confirmation");
+		alert.setMessage("Are you sure?");
+	    alert.setPositiveButton("Yes", pListener);
+		alert.setNegativeButton("No", nListener);
+
+		return alert.create();
+	}
+
+	//This is our positive listener for when the user presses
+	//the yes button
+	DialogInterface.OnClickListener pListener = new DialogInterface.OnClickListener() {
+
+		@Override
+		public void onClick(DialogInterface arg0, int arg1) {
+			// these will be executed when user click Yes button
+			positiveClick();
+		}
+	};
+
+	//This is our negative listener for when the user presses
+	//the no button.
+	DialogInterface.OnClickListener nListener = new DialogInterface.OnClickListener() {
+
+		@Override
+		public void onClick(DialogInterface arg0, int arg1) {
+			// these will be executed when user click No button
+			negativeClick();
+		}
+	};
+	
+    //These two methods are empty, because they will
+	//be overridden
+	protected void positiveClick() 
+	{
+		
+	}
+	protected void negativeClick()
+	{
+		
+	}
+}
